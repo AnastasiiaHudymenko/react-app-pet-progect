2e6133f151115bcab795e9233a5b25c7c6b8cb87
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-03-06 15:40:54 +0200

    add todos

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,18 @@
-// import { Text } from 'components';
-// import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
-// import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import { Text } from 'components';
+import { TodoWrapper, DeleteButton } from './Todo.styled';
+import { RiDeleteBinLine } from 'react-icons/ri';
+// RiEdit2Line EditButton
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ text, num }) => {
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #{num}
+      </Text>
+      <Text>{text}</Text>
+      <DeleteButton type="button">
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,31 @@
-import { Component } from "react";
-// import { nanoid } from 'nanoid';
-import { Text } from "components";
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import { SearchForm, Grid, GridItem, Todo } from 'components';
 // Grid, GridItem, SearchForm, EditForm,Todo
-export class Todos extends Component {
-  render() {
-    return <Text>Todos</Text>;
-  }
-}
+export const Todos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handlSubmit = text => {
+    const todo = {
+      id: nanoid(),
+      text,
+    };
+
+    setTodos(prevTodos => [...prevTodos, todo]);
+  };
+
+  return (
+    <>
+      <SearchForm onSubmit={handlSubmit} />
+      <Grid>
+        {todos.map(({ id, text }, i) => {
+          return (
+            <GridItem key={id}>
+              <Todo text={text} num={i + 1} />
+            </GridItem>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};

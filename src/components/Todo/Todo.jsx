2836fe0c88e18512973634780c82daf101ff9db1
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-03-06 16:28:36 +0200

    save todo in localeStorage and add delete todo

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,14 +3,14 @@ import { TodoWrapper, DeleteButton } from './Todo.styled';
 import { RiDeleteBinLine } from 'react-icons/ri';
 // RiEdit2Line EditButton
 
-export const Todo = ({ text, num }) => {
+export const Todo = ({ text, num, id, deleteTodoByid }) => {
   return (
     <TodoWrapper>
       <Text textAlign="center" marginBottom="20px">
         TODO #{num}
       </Text>
       <Text>{text}</Text>
-      <DeleteButton type="button">
+      <DeleteButton type="button" onClick={() => deleteTodoByid(id)}>
         <RiDeleteBinLine size={24} />
       </DeleteButton>
     </TodoWrapper>
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,17 @@
 import { nanoid } from 'nanoid';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { SearchForm, Grid, GridItem, Todo } from 'components';
 // Grid, GridItem, SearchForm, EditForm,Todo
 export const Todos = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem('todos')) ?? []
+  );
+  // const [isEditing, setIsEditing] = useState(false);
+  // const [currentTodo, setCurrentTodo] = useState([]);
+
+  useEffect(() => {
+    window.localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const handlSubmit = text => {
     const todo = {
@@ -14,17 +22,20 @@ export const Todos = () => {
     setTodos(prevTodos => [...prevTodos, todo]);
   };
 
+  const deleteTodo = idTodo => {
+    const filterTodo = todos.filter(({ id }) => id !== idTodo);
+    setTodos(filterTodo);
+  };
+
   return (
     <>
       <SearchForm onSubmit={handlSubmit} />
       <Grid>
-        {todos.map(({ id, text }, i) => {
-          return (
-            <GridItem key={id}>
-              <Todo text={text} num={i + 1} />
-            </GridItem>
-          );
-        })}
+        {todos.map(({ id, text }, i) => (
+          <GridItem key={id}>
+            <Todo text={text} num={i + 1} id={id} deleteTodoByid={deleteTodo} />
+          </GridItem>
+        ))}
       </Grid>
     </>
   );
